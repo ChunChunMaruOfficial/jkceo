618206996ac55ec8a3bc6872ff77fd5d854ba7b8
ChunChunMaru <c4erkaoui@gmail.com>
2025-04-15 23:54:37 +0300

    slots ist done UwU

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,14 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 
+const emoji = require("emoji-api");
 app.use(cors())
 
 app.get('/getdata', (req, res) => {
-    res.json({ answer: 'heloo nigga'})
+    console.log(emoji.all());
+    let result = emoji.all().filter(v => v.group != 'Flags' && v.subGroup != 'family')
+    result = result.map(v => ({ emoji: v.emoji, name: v.name }))
+    res.json({ answer: result })
 })
 
 app.listen(3001, () => console.log('server is running'))
--- frontend/src/components/Slots/slots.tsx
+++ frontend/src/components/Slots/slots.tsx
@@ -1,9 +1,8 @@
 import { useEffect, useRef, useState } from 'react';
 import styles from './style.module.scss';
-import graph1 from '../../assets/svg/slots/graph1.svg'
-import graph2 from '../../assets/svg/slots/graph2.svg'
-import circlegraph1 from '../../assets/svg/slots/circlegraph1.svg'
-import circlegraph2 from '../../assets/svg/slots/circlegraph2.svg'
+import sword from '../../assets/svg/slots/sword.svg'
+import scales from '../../assets/svg/slots/scales.svg'
+import decor from '../../assets/svg/slots/decoration.svg'
 import getRandom from '../modules/getRandom';
 import axios from 'axios';
 
@@ -14,11 +13,16 @@ export default function Slots() {
     const fisrstslotRef = useRef<HTMLDivElement>(null);
     const secondslotRef = useRef<HTMLDivElement>(null);
     const thirdslotRef = useRef<HTMLDivElement>(null);
-    const [maxTop, setmaxTop] = useState(0)
-    const [servertext, setservertext] = useState('')
+    const [maxTop, setmaxTop] = useState<number>(0)
+    const [serveremoji, setserveremoji] = useState<{ emoji: String, name: string }[]>([])
+    const [silvercoins, setsilvercoins] = useState<number>(500)
+    const [attempts, setattempts] = useState<number>(0)
+    const [winning, setwinning] = useState<string[]>()
     const [slotsspeed, setslotsspeed] = useState<number>(0)
-    const [slotsisactive, setslotsisactive] = useState(false)
-    const pos = useRef({ y: 0, top: 0 });
+    const [slotsisactive, setslotsisactive] = useState<boolean>(false)
+    const pos = useRef({ y: 0, top: 0 })
+
+
     const handleMouseDown = (e: React.MouseEvent<HTMLDivElement>) => {
         pos.current = {
             y: e.clientY,
@@ -33,41 +37,52 @@ export default function Slots() {
         if (!dragRef.current || !parentRef.current || !fisrstslotRef.current || !secondslotRef.current || !thirdslotRef.current) return;
         setmaxTop(parentRef.current.offsetHeight - dragRef.current?.offsetHeight)
         axios.get('http://localhost:3001/getdata')
-        .then((res) => setservertext(res.data.answer))
-        .catch((err) => console.error('Ошибка:', err));
-        console.log(servertext);
-
+            .then((res) => {
+                setserveremoji(res.data.answer);
+                console.log('дата:', res.data.answer)
+            })
     }, [])
 
     useEffect(() => {
         if (slotsisactive) {
-            const times = getRandom(20, 25)
+            setsilvercoins(coins => coins - 30)
+            slotscleaning(fisrstslotRef.current!.children)
+            slotscleaning(secondslotRef.current!.children)
+            slotscleaning(thirdslotRef.current!.children)
+            setwinning([])
+            setattempts(att => att + 1)
+            const times = getRandom(10, 11)
             startslots(fisrstslotRef.current, times)
             setTimeout(() => {
-                startslots(secondslotRef.current, times + getRandom(4, 6))
+                startslots(secondslotRef.current, times + getRandom(1, 2))
             }, 200);
             setTimeout(() => {
-                startslots(thirdslotRef.current, times + getRandom(8, 12))
+                startslots(thirdslotRef.current, times + getRandom(3, 4))
             }, 400);
         }
     }, [slotsisactive])
 
+
     const startslots = (parent: HTMLDivElement | null, times: number) => { // старт каждого слота
-        let count = 0
+        let count: number = 0
         if (parent) {
             let inter = setInterval(() => {
                 const chld = document.createElement('p');
                 setslotsspeed(prev => {
                     chld.classList.add(slotsspeedarray[prev]);
-
                     return prev
                 })
-                chld.textContent = `${getRandom(0, 100)}`;
+                const item = serveremoji[getRandom(0, serveremoji.length)].emoji
+                chld.textContent = `${item}`;
 
-                if (count == times) {
-                    chld.classList.toggle(styles.finalslot);
+
+                if (count >= times) {
                     clearInterval(inter)
+                    chld.classList.toggle(styles.finalslot);
+                }
+                if (count == times) {
                     setslotsisactive(false)
+                    setwinning(win => [...(win ?? []), String(item)]);
                 } else {
                     chld.classList.toggle(styles.activateslots);
 
@@ -75,16 +90,13 @@ export default function Slots() {
                 }
 
                 parent.appendChild(chld)
-            }, getRandom(500, 1000) / (slotsspeed + 1))
+            }, getRandom(950, 1200) / (slotsspeed + 1))
         }
     }
 
-    const slotshide = (array: HTMLCollection) => {
-        Array.from(array).map(v => (v as HTMLParagraphElement).style.opacity = '0')
-    }
-
     const slotscleaning = (array: HTMLCollection) => {
-        Array.from(array).map(v => (v.classList.contains(styles.activateslots) && v.remove(), v.classList.contains(styles.finalslot) && v.classList.replace(styles.finalslot, styles.finalslotend)))
+        Array.from(array).map(v => (v.classList.contains(styles.activateslots)
+            && v.remove(), setTimeout(() => v.classList.contains(styles.finalslotend) && v.remove(), 1500), v.classList.contains(styles.finalslot) && v.classList.replace(styles.finalslot, styles.finalslotend)))
     }
 
     const handleMouseMove = (e: MouseEvent) => {
@@ -94,16 +106,12 @@ export default function Slots() {
 
         if (maxTop - 10 < newTop && fisrstslotRef.current && secondslotRef.current && thirdslotRef.current) { //поочередный старт слотов
             //здесь триггер только на левер, нажатый до конца
-            if (!slotsisactive) {
-                slotscleaning(fisrstslotRef.current.children)
-                slotscleaning(secondslotRef.current.children)
-                slotscleaning(thirdslotRef.current.children)
+            if (!slotsisactive) { //если не активен
+
                 setslotsisactive(true)
-            } else {
-                setslotsspeed(slotsspeed + 1)
-                slotshide(fisrstslotRef.current.children)
-                slotshide(secondslotRef.current.children)
-                slotshide(thirdslotRef.current.children)
+
+            } else { //во время крутки
+                slotsspeed < 4 && setslotsspeed(slotsspeed + 1)
             }
         }
 
@@ -144,18 +152,18 @@ export default function Slots() {
                     </div>
                     <div>
                         <div>
-                            <img src={graph1} alt="" />
+                            <img src={scales} alt="" />
                             {[...Array(4)].map(() => (<span className={styles.point}></span>))}
-                            <img src={circlegraph1} alt="" />
+                            <img src={sword} className={styles.rotated} alt="" />
                         </div>
                         <div className={styles.headtext}>
                             <h2>JKCEO </h2>
-                            <h2>{servertext}</h2>
+                            <h2>MACHINE</h2>
                         </div>
                         <div>
-                            <img src={circlegraph2} alt="" />
+                            <img src={sword} alt="" />
                             {[...Array(4)].map(() => (<span className={styles.point}></span>))}
-                            <img src={graph2} alt="" />
+                            <img src={scales} className={styles.rotated} alt="" />
                         </div>
                     </div>
                     <div className={styles.sidepart}>
@@ -165,17 +173,35 @@ export default function Slots() {
                 </div>
                 <div>
                     <div className={styles.main}>
-                        <div className={styles.display}>
-                            <span></span>
-                            <div  ><div ref={fisrstslotRef} className={styles.slot}></div></div>
-                            <span></span>
-                            <div><div ref={secondslotRef} className={styles.slot}></div></div>
-                            <span></span>
-                            <div><div ref={thirdslotRef} className={styles.slot}></div></div>
-                            <span></span>
+                        <div>
+                            <div className={styles.display}>
+                                <img src={decor} alt="" />
+                                <span></span>
+                                <div><div ref={fisrstslotRef} className={styles.slot}></div></div>
+                                <span></span>
+                                <div><div ref={secondslotRef} className={styles.slot}></div></div>
+                                <span></span>
+                                <div><div ref={thirdslotRef} className={styles.slot}></div></div>
+                                <span></span>
+                                <img src={decor} alt="" />
+                            </div>
+                        </div>
+                        <div className={styles.botompanel}>
+                            <div>
+                                <span>Silver Coin</span>
+                                <span>{silvercoins}</span>
+                            </div>
+                            <div className={styles.botomcenter}>
+                                {[...Array(4)].map(() => (<p className={styles.microbolt}></p>))}
+                                {winning?.map(v => (v))}
+                            </div>
+                            <div>
+                                <span>Attempts</span>
+                                <span>{attempts}</span>
+                            </div>
+                            <div> {[...Array(4)].map((_, i) => (<p style={{ background: slotsspeed <= i  ? "#F0EFEB" : '#A5A58D' }} className={styles.microbolt}></p>))}</div>
                         </div>
                     </div>
-
                     <div className={styles.lever}>
                         <div className={styles.sidepart}>
                             <span className={styles.microbolt}></span>
